cf7f30ee144e5ae843a78447a3eabbd5959e11a7
Utkarsh Pandey <68511329+pandeyutkarsh@users.noreply.github.com>
2022-01-02 21:48:10 +0530

    Create Fibonacci.cpp

---
+++ Day 2/Fibonacci.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int fab(int n){
+    if(n == 0 || n == 1)
+        return n;
+
+    return (fab(n-1) + fab(n-2));    
+}
+
+int main() {
+  cout <<fab(6);
+} 
